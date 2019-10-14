49d62d2d6c6c1ae8db12038c8581999602385cf5
Paul Anderson <hpcprofessional@gmail.com>
2019-10-14 17:03:57 -0600

    pdk new module + new task

--- scripts/cam-tool.sh
+++ scripts/cam-tool.sh
@@ -6,6 +6,9 @@ ALL_RES='/metrics/v1/mbeans/puppetlabs.puppetdb.population:name=num-resources'
 AVG_RES_PER_NODE='/metrics/v1/mbeans/puppetlabs.puppetdb.population:name=avg-resources-per-node'
 PCT_DUP_RES='/metrics/v1/mbeans/puppetlabs.puppetdb.population:name=pct-resource-dupes'
 
+#Need tasks/plans info
+#Need specific resource counts
+
 TARGET=$LOCAL
 
 echo -n "Nodes managed:"
